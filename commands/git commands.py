577beba6d511e1e70fc12c8a9bb86824b84cc7be
Lucas Newman <Lucas.newman140@gmail.com>
2024-12-20 08:49:42 +1100

    updating git commands

--- commands/git commands.py
+++ commands/git commands.py
@@ -12,7 +12,13 @@ SCRIPT_PATHS = {
     "Clone Repository": GIT_COMMANDS_PATH / "git clone repo.py",
     "Pull Repository": GIT_COMMANDS_PATH / "git pull repo.py",
     "Create Repository": GIT_COMMANDS_PATH / "git create repo.py",
+    "commit Repository": GIT_COMMANDS_PATH / "git commit repo.py",
+    "Init Repository": GIT_COMMANDS_PATH / "git init repo.py",
+    "Push Repository": GIT_COMMANDS_PATH / "git push to remote.py",
+    "Status check": GIT_COMMANDS_PATH / "git status check.py",
+    "Install GIT": GIT_COMMANDS_PATH / "install git.py",
 }
+
 
 def launch_script(script_name):
     """Launch the selected script."""
@@ -21,6 +27,7 @@ def launch_script(script_name):
         subprocess.Popen([sys.executable, str(script_path)])
     else:
         tk.messagebox.showerror("Error", f"Script '{script_name}' not found!")
+
 
 # Main UI setup
 root = tk.Tk()
@@ -38,8 +45,15 @@ dropdown.configure(bg="white", fg="#171717")
 dropdown.pack(pady=10)
 
 # Button to execute the selected script
-run_button = tk.Button(root, text="Run Command", command=lambda: launch_script(command_var.get()), 
-                        bg="white", fg="#171717", borderwidth=2, relief="groove")
+run_button = tk.Button(
+    root,
+    text="Run Command",
+    command=lambda: launch_script(command_var.get()),
+    bg="white",
+    fg="#171717",
+    borderwidth=2,
+    relief="groove",
+)
 run_button.pack(pady=20)
 
 root.mainloop()
